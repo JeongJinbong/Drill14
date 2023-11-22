fe642be56114b087929079992f782fe92b878c43
JeongJinbong <jbong0104@naver.com>
2023-11-22 19:00:08 +0900

    Drill 14 제출

---
+++ ball.py
@@ -0,0 +1,38 @@
+from pico2d import *
+import game_world
+import game_framework
+import random
+
+class Ball:
+    image = None
+
+    def __init__(self, x = None, y = None):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x = x if x else random.randint(100, 1180)
+        self.y = y if y else random.randint(100, 924)
+
+    def set_background(self, bg):
+        self.bg = bg
+
+
+    def draw(self):
+        sx, sy = self.x - self.bg.window_left, self.y - self.bg.window_bottom
+        self.image.draw(sx, sy)
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return self.x - self.bg.window_left- 10, self.y- self.bg.window_bottom - 10, self.x- self.bg.window_left + 10, self.y- self.bg.window_bottom + 10
+
+    def handle_collision(self, group, other):
+        match group:
+            case 'boy:ball':
+                game_world.remove_object(self) # 소년이 볼을 소유하도록.
+                pass
+            case 'zombie:ball':
+                other.ball = self
+
+
